b76d6992bcbbc75da76c3349798fef94ac7bfaa7
punnfect <sp.kool@yahoo.com>
2025-08-30 20:54:46 -0500

    First Commit

---
+++ src/main/java/com/github/punnfect/family_recipe/FamilyRecipeSiteApplication.java
@@ -0,0 +1,13 @@
+package com.github.punnfect.family_recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyRecipeSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyRecipeSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/punnfect/family_recipe/FamilyRecipeSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.punnfect.family_recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyRecipeSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
